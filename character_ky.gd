64308e856e4295d3ee7f0aa936c6a6aa9f7e5309
optimisticlucio <73440828+optimisticlucio@users.noreply.github.com>
2023-08-10 06:18:29 +0300

    Created basic Godot Project
    as of the moment we have two scenes - one with Ky's idle animation, and another basic screen for him to stand in.

---
+++ character_ky.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
