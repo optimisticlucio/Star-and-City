1f71114e2dee54e94604f605e38438cc1dbf095f
Yossipossi <yossipossi770@gmail.com>
2023-08-30 05:13:57 -0700

    Add support for two healthbars.

--- godot/scripts/generic/healthbar.gd
+++ godot/scripts/generic/healthbar.gd
@@ -2,6 +2,15 @@ class_name health_bar extends ProgressBar
 
 @onready var scene = get_node("..")
 
+# Set the max and current health for each character.
 func _ready():
-	self.max_value = scene.player1.MAX_HEALTH
-	self.value = scene.player1.MAX_HEALTH
+	var max_health: int
+		
+	# NOTE: Probably not the most idiomatic way to achieve this.
+	# At some point, probably worth looking into an alternative.
+	match name:
+		"Healthbar1": max_health = scene.player1.MAX_HEALTH
+		"Healthbar2": max_health = scene.player2.MAX_HEALTH
+	
+	self.max_value = max_health
+	self.value = max_health
