88ac83d8930b63f37d33bec7ecd2b94706a11333
Yossipossi <yossipossi770@gmail.com>
2023-10-13 11:01:51 -0700

    Clean up lib.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,51 +4,11 @@ pub mod generic;
 pub mod prelude;
 pub mod utils;
 
-use crate::prelude::*;
+use godot::prelude::{ExtensionLibrary, gdextension};
 
-use godot::engine::{Sprite2D, Sprite2DVirtual};
+// Declare the extension trait, which allows for Godot's bindings to work.
+pub struct MyExtension;
 
-// This stuff is from the tutorial, mostly to understand how
-// the rust extension even works.
-#[derive(GodotClass)]
-#[class(base=Sprite2D)]
-struct Player {
-    speed: f64,
-    angular_speed: f64,
-
-    #[base]
-    sprite: Base<Sprite2D>,
-}
-
-#[godot_api]
-impl Sprite2DVirtual for Player {
-    fn init(sprite: Base<Sprite2D>) -> Self {
-        godot_print!("Hello, world!"); // Prints to the Godot console
-
-        Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
-            sprite,
-        }
-    }
-
-    fn physics_process(&mut self, delta: f64) {
-        // GDScript code:
-        //
-        // rotation += angular_speed * delta
-        // var velocity = Vector2.UP.rotated(rotation) * speed
-        // position += velocity * delta
-
-        self.sprite.rotate((self.angular_speed * delta) as f32);
-
-        let rotation = self.sprite.get_rotation();
-        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
-        self.sprite.translate(velocity * delta as f32);
-
-        // or verbose:
-        // self.sprite.set_position(
-        //     self.sprite.get_position() + velocity * delta as f32
-        // );
-    }
-}
-//------------------------------------------------------------------
+// Implement it.
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
--- rust/src/prelude.rs
+++ rust/src/prelude.rs
@@ -1,13 +1,6 @@
 // Use the Godot prelude implicitly across the crate.
 pub use godot::prelude::*;
 
-// Declare the extension trait, which allows for Godot's bindings to work.
-pub struct MyExtension;
-
-// Implement it.
-#[gdextension]
-unsafe impl ExtensionLibrary for MyExtension {}
-
 pub use crate::errors::{Error, Result};
 
 pub mod math {
