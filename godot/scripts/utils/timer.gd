fa8ab35dfce9108fb3cd9d8c06be497ea1865503
Yossipossi <yossipossi770@gmail.com>
2023-10-31 18:11:27 -0700

    Redid timer code

--- godot/scripts/utils/timer.gd
+++ godot/scripts/utils/timer.gd
@@ -2,14 +2,12 @@ class_name DTimer
 
 const TICKS_PER_SECOND := 60
 
-var infinite := false # Set to true when you want the timer to not do anything.
-var tick_count := TICKS_PER_SECOND # Counts down to 0 from TICKS_PER_SECOND.
+var tick_count := 0
 var reset_seconds := 99
 var seconds := 99
 
-func _init(sec := 99, inf := false):
+func _init(sec := 99):
 	reset_seconds = sec
-	infinite = inf
 	reset_clock()
 
 # adds i to the default timer. Returns the new default time.
@@ -25,15 +23,9 @@ func add_to_clock(i: int) -> int:
 func reset_clock():
 	seconds = reset_seconds
 
-# Ticks down once, returns the current amount of seconds, and stops ticking at 
-# 0 seconds. Returns -1 if time is set to infinite.
+# Advances the timer by one tick, and advanced by one second every X
+# ticks, where X is the TICKS_PER_SECOND.
 func tick() -> int:
-	if !infinite:
-		if seconds: # if seconds > 0
-			tick_count -= 1
-			if !tick_count:
-				tick_count = TICKS_PER_SECOND
-				seconds -= 1
-		return seconds
-	else:
-		return -1
+	tick_count += 1
+	seconds -= int((tick_count % TICKS_PER_SECOND) == 0)
+	return seconds
