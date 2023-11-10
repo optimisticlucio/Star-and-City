152a968655bd60bff91fbb08d65e2b326e057264
Yossipossi <yossipossi770@gmail.com>
2023-11-09 17:25:53 -0800

    Changed EGO gift indenting to improve readability

--- godot/scripts/generic/ego_gift.gd
+++ godot/scripts/generic/ego_gift.gd
@@ -43,32 +43,86 @@ enum Gift {
 static var EGO_LIST = {
 # NOTE: You need to use lambda functions here because... fuck idk. Godot.
 	Gift.DUMMY: EgoGift.new(),
-	Gift.SHORTTIME: EgoGift.new("The 13th Toll", Gift.SHORTTIME,
-		null, "Shorten Timer", "Reduce Round Timer By 30 Seconds", 
-		func(x): EGOGifts.lower_timer(x, -30)),
-	Gift.FASTWALK: EgoGift.new("Obsession", Gift.FASTWALK,
-		null, "Faster Walk", "Double Walking Speed", 
-		func(x): EGOGifts.increase_speed_by(x, 2)),
-	Gift.METERTHEFT: EgoGift.new("Melty Eyeball", Gift.METERTHEFT,
-		null, "Steal Meter", "On Hit, Steal From Opponent's Meter",
-		func(x): x.extrafunc.add_on_hitting(func(i): EGOGifts.steal_meter(i, 2000))),
-	Gift.AIRDASH: EgoGift.new("Illusory Hunt", Gift.AIRDASH,
-		null, "Air Dash", "Grants Mid-Air Dash"),
-	Gift.RHYTHM_GAME: EgoGift.new("Fervent Beats", Gift.RHYTHM_GAME,
-		null, "Timed Buff", "Increase Damage Timed To Heartbeat"),
-	Gift.JESUS_SKULL: EgoGift.new("Penitence", Gift.JESUS_SKULL,
-		null, "Super Move", "Gain a Powerful Super Move"),
-	Gift.CARMEN: EgoGift.new("Apostles", Gift.CARMEN,
-		null, "Gain Follower", "Gain a Follower to Attack Opponent"),
-	Gift.TINY_BIRD: EgoGift.new("Punishment", Gift.TINY_BIRD,
-		null, "Counter Boost", "Counterhits Deal Extra Damage"),
-	Gift.HELLO: EgoGift.new("Hello?", Gift.HELLO,
-		null, "Copy Move", "Copy an Opponent's Super Move"),
-	Gift.CENSORED: EgoGift.new("CENSORED", Gift.CENSORED,
-		null, "Hide Character", "Briefly Hide Your Moves"),
-	Gift.PARRY: EgoGift.new("Well-Worn Parasol", Gift.PARRY,
-		null, "Perfect Block", "Block Attack Perfectly to Negate Damage",
-		func(x): x.extrafunc.add_on_block(func(i): EGOGifts.perfect_block(i, 20)))
+	Gift.SHORTTIME: EgoGift.new(
+		"The 13th Toll",
+		Gift.SHORTTIME,
+		null,
+		"Shorten Timer",
+		"Reduce Round Timer By 30 Seconds", 
+		func(x): EGOGifts.lower_timer(x, -30)
+	),
+	Gift.FASTWALK: EgoGift.new(
+		"Obsession",
+		Gift.FASTWALK,
+		null,
+		"Faster Walk",
+		"Double Walking Speed", 
+		func(x): EGOGifts.increase_speed_by(x, 2)
+	),
+	Gift.METERTHEFT: EgoGift.new(
+		"Melty Eyeball",
+		Gift.METERTHEFT,
+		null,
+		"Steal Meter",
+		"On Hit, Steal From Opponent's Meter",
+		func(x): x.extrafunc.add_on_hitting(func(i): EGOGifts.steal_meter(i, 2000))
+	),
+	Gift.AIRDASH: EgoGift.new(
+		"Illusory Hunt",
+		Gift.AIRDASH,
+		null,
+		"Air Dash",
+		"Grants Mid-Air Dash"
+	),
+	Gift.RHYTHM_GAME: EgoGift.new(
+		"Fervent Beats",
+		Gift.RHYTHM_GAME,
+		null,
+		"Timed Buff",
+		"Increase Damage Timed To Heartbeat"
+	),
+	Gift.JESUS_SKULL: EgoGift.new(
+		"Penitence",
+		Gift.JESUS_SKULL,
+		null,
+		"Super Move",
+		"Gain a Powerful Super Move"
+	),
+	Gift.CARMEN: EgoGift.new(
+		"Apostles",
+		Gift.CARMEN,
+		null,
+		"Gain Follower",
+		"Gain a Follower to Attack Opponent"
+	),
+	Gift.TINY_BIRD: EgoGift.new(
+		"Punishment", Gift.TINY_BIRD,
+		null,
+		"Counter Boost",
+		"Counterhits Deal Extra Damage"
+	),
+	Gift.HELLO: EgoGift.new(
+		"Hello?",
+		Gift.HELLO,
+		null,
+		"Copy Move",
+		"Copy an Opponent's Super Move"
+	),
+	Gift.CENSORED: EgoGift.new(
+		"CENSORED",
+		Gift.CENSORED,
+		null,
+		"Hide Character",
+		"Briefly Hide Your Moves"
+	),
+	Gift.PARRY: EgoGift.new(
+		"Well-Worn Parasol",
+		Gift.PARRY,
+		null,
+		"Perfect Block",
+		"Block Attack Perfectly to Negate Damage",
+		func(x): x.extrafunc.add_on_block(func(i): EGOGifts.perfect_block(i, 20))
+	),
 }
 
 static func get_ego(gift: Gift):
