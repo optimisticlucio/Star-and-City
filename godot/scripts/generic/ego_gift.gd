78bbe1016680694fad00de3a7e5204affb6f030a
Yossipossi <yossipossi770@gmail.com>
2023-11-09 17:29:03 -0800

    Remove "corresponding_enum" since that information is necessary to even get that in the first place. Readd if it becomes an issue.

--- godot/scripts/generic/ego_gift.gd
+++ godot/scripts/generic/ego_gift.gd
@@ -1,23 +1,19 @@
 class_name EGOGifts
 
-
 class EgoGift:
 	var name: String
-	var representing_enum: Gift
 	var image: Resource # TODO
 	var short_desc: String
 	var long_desc: String
 	var init_function: Callable
 	
 	func _init(name := "TEST",
-		representing_enum := Gift.DUMMY,
 		image: Resource = null,
 		short_desc := "Missing Data",
 		long_desc := "Should not appear during gameplay.",
 		init_function := func(): print("ERROR - TEST EGO IN GAMEPLAY")
 	):
 		self.name = name
-		self.representing_enum = representing_enum
 		self.image = image
 		self.short_desc = short_desc
 		self.long_desc = long_desc
@@ -45,79 +41,79 @@ static var EGO_LIST = {
 	Gift.DUMMY: EgoGift.new(),
 	Gift.SHORTTIME: EgoGift.new(
 		"The 13th Toll",
-		Gift.SHORTTIME,
 		null,
 		"Shorten Timer",
 		"Reduce Round Timer By 30 Seconds", 
 		func(x): EGOGifts.lower_timer(x, -30)
 	),
+	
 	Gift.FASTWALK: EgoGift.new(
 		"Obsession",
-		Gift.FASTWALK,
 		null,
 		"Faster Walk",
 		"Double Walking Speed", 
 		func(x): EGOGifts.increase_speed_by(x, 2)
 	),
+	
 	Gift.METERTHEFT: EgoGift.new(
 		"Melty Eyeball",
-		Gift.METERTHEFT,
 		null,
 		"Steal Meter",
 		"On Hit, Steal From Opponent's Meter",
 		func(x): x.extrafunc.add_on_hitting(func(i): EGOGifts.steal_meter(i, 2000))
 	),
+	
 	Gift.AIRDASH: EgoGift.new(
 		"Illusory Hunt",
-		Gift.AIRDASH,
 		null,
 		"Air Dash",
 		"Grants Mid-Air Dash"
 	),
+	
 	Gift.RHYTHM_GAME: EgoGift.new(
 		"Fervent Beats",
-		Gift.RHYTHM_GAME,
 		null,
 		"Timed Buff",
 		"Increase Damage Timed To Heartbeat"
 	),
+	
 	Gift.JESUS_SKULL: EgoGift.new(
 		"Penitence",
-		Gift.JESUS_SKULL,
 		null,
 		"Super Move",
 		"Gain a Powerful Super Move"
 	),
+	
 	Gift.CARMEN: EgoGift.new(
 		"Apostles",
-		Gift.CARMEN,
 		null,
 		"Gain Follower",
 		"Gain a Follower to Attack Opponent"
 	),
+	
 	Gift.TINY_BIRD: EgoGift.new(
-		"Punishment", Gift.TINY_BIRD,
+		"Punishment",
 		null,
 		"Counter Boost",
 		"Counterhits Deal Extra Damage"
 	),
+	
 	Gift.HELLO: EgoGift.new(
 		"Hello?",
-		Gift.HELLO,
 		null,
 		"Copy Move",
 		"Copy an Opponent's Super Move"
 	),
+	
 	Gift.CENSORED: EgoGift.new(
 		"CENSORED",
-		Gift.CENSORED,
 		null,
 		"Hide Character",
 		"Briefly Hide Your Moves"
 	),
+	
 	Gift.PARRY: EgoGift.new(
 		"Well-Worn Parasol",
-		Gift.PARRY,
 		null,
 		"Perfect Block",
 		"Block Attack Perfectly to Negate Damage",
